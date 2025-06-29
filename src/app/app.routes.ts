bcb3395fafe15403499aaa371ad1ad01146798a0
whitelightningdev <danielmommsen@hotmail.com>
2025-06-29 14:36:14 +0200

    `Removed breadcrumb component and related files from the project.`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,5 @@
 import { Routes } from '@angular/router';
-import { Homepage } from './pages/homepage/homepage';
-import { Bookingpage } from './pages/bookingpage/bookingpage';
-import { Aboutus } from './pages/aboutus/aboutus';
+
 export const routes: Routes = [
-  {
-    path: '',
-    component: Homepage,
-    data: { breadcrumb: 'Home' }
-  },
-  {
-    path: 'make-a-booking',
-    component: Bookingpage,
-    data: { breadcrumb: 'Make a Booking' }
-  },
-  {
-    path: 'about-us',
-    component: Aboutus,
-    data: { breadcrumb: 'About Us' }
-  }
+
 ];
