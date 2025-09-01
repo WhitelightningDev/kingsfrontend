84cdfa89c359307d89361aefd9d0176bd6289265
Daniel Mommsen <danielmommsen@hotmail.com>
2025-09-01 18:08:39 +0200

    `Added BookingPage component and updated routes and navbar to include it`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './pages/homepage/homepage';
+import { BookingPage } from './pages/booking-page/booking-page';
+
 export const routes: Routes = [
   {
     path: '', component: Homepage
+  },
+  {
+    path: 'Book-a-wash', component: BookingPage
   }
 ];
